5046d9bb94be17d74637526e48c96ca71f801266
Shelby Hanna <144790272+Shanna36@users.noreply.github.com>
2026-03-31 15:38:37 +0100

    Update AnimatorStartOffset.cs

--- Assets/Scripts/AnimatorStartOffset.cs
+++ Assets/Scripts/AnimatorStartOffset.cs
@@ -11,8 +11,7 @@ public class AnimatorStartOffset : MonoBehaviour
 
         float t = normalizedStartTime < 0f ? Random.value : normalizedStartTime;
 
-        // Play the current default state at a normalized time offset
         anim.Play(0, 0, t);
-        anim.Update(0f); // apply immediately
+        anim.Update(0f); 
     }
 }
